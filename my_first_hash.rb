6faaba096d6751c28ddebf55e7a7d572b6ffbd0e
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-10-29 14:10:37 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,39 +1,7 @@
 def my_hash
- your_hash = { "name" => "Josh", "age" => 18 }
+ your_hash = { "name" => "Josh"}
 
 end
 
 
-def shipping_manifestshipping_manifest 
-  the_manifestb= { 
-    "whale bone corsets" => 5, 
-    "porcelain vases" => 2, 
-    "oil paintings" => 3 
-  }
 
-
-
-end
-
-def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  shipping_manifest["oil paintings"]
-
-end
-
-def adding
-  shipping_manifest = {
-  "whale bone corsets" => 5, 
-  "porcelain vases" => 2, 
-  "oil paintings" => 3
-  }
-  shipping_manifest["muskets"] = 2
-
-  shipping_manifest["gun powder"] = 4
-
-  shipping_manifest
-end
